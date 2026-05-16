5e824dcd6ae50a967c9667365fd77df441e2b37b
nguyencon2309 <connoone0923@gmail.com>
2026-05-16 14:06:37 +0700

    bitmap image

--- app/src/main/java/com/datn/smarttray/MainActivity.java
+++ app/src/main/java/com/datn/smarttray/MainActivity.java
@@ -8,34 +8,76 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.FileDescriptor;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button galleryBtn, cameraBtn;
+    Uri image_uri;
+
+
 
     ActivityResultLauncher<Intent> galleryActivityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
-                    Uri image_url = result.getData().getData();
-                    imageView.setImageURI(image_url);
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData()!=null) {
+                        Uri selectedImageUri = result.getData().getData();
+                        if(selectedImageUri != null){
+                            image_uri = result.getData().getData();
+                            Bitmap inputImage = uriToBitmap(image_uri);
+                            Bitmap rotated = rotateBitmap(inputImage);
+                            imageView.setImageBitmap(rotated);
+                        }
+
+                    }
+                }
+
+            }
+    );
+
+    //TODO capture the image using camera and display it
+    ActivityResultLauncher<Intent> cameraActivityResultLauncher  = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK
+
+                    ){
+                        Bitmap inputImage = uriToBitmap(image_uri);
+                        Bitmap rotated = rotateBitmap(inputImage);
+                        imageView.setImageBitmap(rotated);
+                    }
                 }
             }
     );
 
 
-    ImageView imageView;
-    Button galleryBtn, cameraBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,18 +88,7 @@ public class MainActivity extends AppCompatActivity {
         cameraBtn = findViewById(R.id.button2);
 
         //TODO ask for permission of camera upon first launch of application
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(android.Manifest.permission.CAMERA) ==
-                    PackageManager.PERMISSION_DENIED ||
-                    checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                            == PackageManager.PERMISSION_DENIED) {
-                String[] permission = {
-                        android.Manifest.permission.CAMERA,
-                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-                };
-                requestPermissions(permission, 112);
-            }
-        }
+
 
 
         galleryBtn.setOnClickListener(new View.OnClickListener() {
@@ -65,10 +96,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 galleryActivityResultLauncher.launch(galleryIntent);
-//                Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//                startActivityForResult(cameraIntent, 1);
+
             }
         });
+
+
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -91,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    Uri image_uri;
+
     //TODO opens camera so that user can capture image
 
     private void openCamera() {
@@ -119,16 +151,49 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-    //TODO capture the image using camera and display it
-    ActivityResultLauncher<Intent> cameraActivityResultLauncher  = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if(result.getResultCode() == RESULT_OK){
-                        imageView.setImageURI(image_uri);
-                    }
-                }
+
+
+    //TODO takes URI of the image and returns bitmap
+    private Bitmap uriToBitmap(Uri selectedFileUri) {
+        try {
+            ParcelFileDescriptor parcelFileDescriptor =
+                    getContentResolver().openFileDescriptor(selectedFileUri, "r");
+            if (parcelFileDescriptor == null) {
+                return null;
             }
-    );
+            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+            Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+
+            parcelFileDescriptor.close();
+            return image;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  null;
+    }
+
+    //TODO rotate image if image captured on samsung devices
+    //TODO Most phone cameras are landscape, meaning if you take the photo in portrait, the resulting photos will be rotated 90 degrees.
+    @SuppressLint("Range")
+    public Bitmap rotateBitmap(Bitmap input){
+        if (input == null) {
+            return null;
+        }
+        String[] orientationColumn = {MediaStore.Images.Media.ORIENTATION};
+        Cursor cur = getContentResolver().query(image_uri, orientationColumn, null, null, null);
+        int orientation = -1;
+        if (cur != null && cur.moveToFirst()) {
+            orientation = cur.getInt(cur.getColumnIndex(orientationColumn[0]));
+            cur.close();
+        }
+        Log.d("tryOrientation",orientation+"");
+        Matrix rotationMatrix = new Matrix();
+        rotationMatrix.setRotate(orientation);
+        Bitmap cropped = Bitmap.createBitmap(input,0,0, input.getWidth(), input.getHeight(), rotationMatrix, true);
+        return cropped;
+    }
+
+
+
 }
+
